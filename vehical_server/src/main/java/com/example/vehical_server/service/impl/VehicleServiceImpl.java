a974e8a0aedcc006151d82f0d30c65e2d41168ff
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2024-06-25 13:53:19 +0530

    complete vehical

---
+++ vehical_server/src/main/java/com/example/vehical_server/service/impl/VehicleServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.vehical_server.service.impl;
+
+import com.example.vehical_server.dto.VehicleDTO;
+import com.example.vehical_server.entity.Vehicle;
+import com.example.vehical_server.repo.VehicleRepo;
+import com.example.vehical_server.service.VehicleService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    private VehicleRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public VehicleDTO saveVehicle(VehicleDTO dto) {
+        if (repo.existsById(dto.getId())){
+            new RuntimeException("all ready exits");
+        }
+        return mapper.map(repo.save(mapper.map(dto, Vehicle.class)),VehicleDTO.class);
+    }
+
+    @Override
+    public VehicleDTO updateVehicle(VehicleDTO dto) {
+        if (repo.existsById(dto.getId())){
+            new RuntimeException("all ready exits");
+        }
+        return mapper.map(repo.save(mapper.map(dto, Vehicle.class)),VehicleDTO.class);
+
+    }
+
+
+    public List<VehicleDTO> getAllVehicle() {
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<VehicleDTO>>() {
+        }.getType()); }
+}
